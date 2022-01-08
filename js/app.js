6e2e5b8977ab55edba9f414b55927c5b74b3af56
Raul <raulfernan000@gmail.com>
2022-01-08 19:25:08 +0100

    small fixes

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@ import { inputPassword } from "./components/InputPassword.js";
 import { readMore } from "./components/ReadMore.js";
 
 let app = new Vue({
-  /* html */
-  template: ``,
   el: "#app",
   data: {
     title: "Vue - Basics Exercise",
